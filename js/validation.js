d9fed6899acb33b7a78356ba23e4b55211e392c8
em40250 <em40250@ubt-uni.net>
2020-05-06 06:20:03 +0200

    contact input - onblur function add

--- js/validation.js
+++ js/validation.js
@@ -2,14 +2,46 @@ const name = document.getElementById('name');
 const email = document.getElementById('email');
 const komenti = document.getElementById('komenti');
 const form = document.getElementById('form');
-var index = 0;
+
+var indexN = 0;
+var indexE = 0;
+var indexK = 0;
 
 form.addEventListener('submit', (e) => {
     checkInput();
-    if( index < 3) {
+    if( indexN == 0 || indexE == 0 || indexK == 0 ) {
         e.preventDefault();
     }
 });
+
+name.onblur = function(){
+    const nameValue = name.value.trim();
+    if (nameValue === '') {
+        setErrorFor(name, 'Kjo fushe nuk mund te lihet e zbrazet!');
+    }else if(nameValue.length < 3 ){
+        setErrorFor(name, 'Emri duhet ti ket meshum se tri shkonja!')
+    } else {
+        setSuccessFor(name);
+    }
+}
+
+email.onblur = function (){
+    const emailValue = email.value.trim();
+    if (emailValue === '') {
+        setErrorFor(email, 'Kjo fushe nuk mund te lihet e zbrazet!');
+    } else {
+        setSuccessFor(email);
+    }
+}
+
+komenti.onblur = function(){
+    const komentiValue = komenti.value.trim();
+    if (komentiValue === '') {
+        setErrorFor(komenti, 'Kjo fushe nuk mund te lihet e zbrazet!');
+    }else {
+        setSuccessFor(komenti);
+    }
+}
 
 function checkInput() {
     const nameValue = name.value.trim();
@@ -17,27 +49,22 @@ function checkInput() {
     const komentiValue = komenti.value.trim();
 
     if (nameValue === '') {
-        setErrorFor(name, 'Kjo fushe nuk mund te lihet e zbrazet!');
+    }else if(nameValue.length < 3 ){
     } else {
-        setSuccessFor(name);
-        index++;
+        indexN++;
     }
 
     if (emailValue === '') {
-        setErrorFor(email, 'Kjo fushe nuk mund te lihet e zbrazet!');
     } else {
-        setSuccessFor(email);
-        index++;
+        indexE++;
     }
 
     if (komentiValue === '') {
-        setErrorFor(komenti, 'Kjo fushe nuk mund te lihet e zbrazet!');
     }else {
-        setSuccessFor(komenti);
-        index++;
+        indexK++;
     }
 
-    if( index == 3 ){
+    if( indexN > 0 && indexE > 0 && indexK > 0 ){
         window.alert('Mesazhi u dergua me sukses!');
     }
 }
