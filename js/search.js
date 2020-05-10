77e95d86db9d18d0aa43eb73ece76a8ac2da9d68
em40250 <em40250@ubt-uni.net>
2020-05-10 19:13:26 +0200

    te gjitha veturat page - filterProblem!!

---
+++ js/search.js
@@ -0,0 +1,17 @@
+function kerko(){
+    var veturat = document.getElementsByClassName('veturat');
+
+    var titulli = document.getElementsByClassName('titulli');
+
+    var input = document.getElementById('search');
+    input = input.toLowerCase;
+
+    for (var i = 0; i < veturat.length; i++) {  
+        if (!titulli[i].innerHTML.toLowerCase().includes(input)) { 
+            veturat[i].style.display="none"; 
+        } 
+        else { 
+            veturat[i].style.display="";                  
+        } 
+    } 
+}
