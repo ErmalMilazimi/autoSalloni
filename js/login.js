bf85ca5752c16091db65df7ed80fc28ee6c9c02a
em40250 <em40250@ubt-uni.net>
2020-05-13 02:10:26 +0200

    login window - some fixes, but not done!

--- js/login.js
+++ js/login.js
@@ -6,8 +6,55 @@ document.querySelector('.close').addEventListener('click',function(){
     document.querySelector('.popup-model').style.display='none';
 });
 
-const name = document.getElementById('name');
-const form = document.getElementById('form');
+document.getElementById('logp').addEventListener('click',function(){
+    document.querySelector('.register-content').classList.add('hidden');
+    document.querySelector('.login-content').classList.remove('hidden');
+
+    document.getElementById('logform').addEventListener('submit', (e) => {
+        if( !isName() ){
+            e.preventDefault();
+        }
+
+        name.onblur = function(){
+            isName();
+        }
+
+        function isName(){
+            var emri = document.getElementById('nameLog').value.trim();
+
+            if (emri === '') {
+                setErrorFor(name, 'Kjo fushe nuk mund te lihet e zbrazet!');
+                return false;
+            } else {
+                return true;
+            }
+        }
+    });
+
+});
+
+document.getElementById('regp').addEventListener('click',function(){
+    document.querySelector('.register-content').classList.remove('hidden');
+    document.querySelector('.login-content').classList.add('hidden');
+});
+
+function setErrorFor(input, message) {
+    const inputKlasa = input.parentElement;
+    const small = inputKlasa.querySelector('small');
+
+    small.innerText = message;
+
+    inputKlasa.className = 'inputKlasa error';
+}
+
+// function setSuccessFor(input){
+//     const inputKlasa = input.parentElement;
+//     inputKlasa.className = 'inputKlasa success';
+// }
+
+
+// const name = document.getElementById('name');
+// const form = document.getElementById('form');
 
 // form.addEventListener('submit', (e) => {
 //     if( !isName() ) {
@@ -22,9 +69,9 @@ const form = document.getElementById('form');
 // function isName(){
 //     const nameValue = name.value.trim();
 
-//     if (nameValue === '') {
-//         setErrorFor(name, 'Kjo fushe nuk mund te lihet e zbrazet!');
-//         return false;
+    // if (nameValue === '') {
+    //     setErrorFor(name, 'Kjo fushe nuk mund te lihet e zbrazet!');
+    //     return false;
 //     }else if(nameValue.length < 3 ){
 //         setErrorFor(name, 'Kjo fushe duhet ti kete tre apo meshum shkonja!')
 //         return false;
